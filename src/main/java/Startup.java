1281c842bc9bf9136a1640f66aadb0b23f692d5b
ncthbrt <nick@cuthbert.co.za>
2016-04-23 19:36:36 +0200

    Added web.config and modified gradle file

--- src/main/java/Startup.java
+++ src/main/java/Startup.java
@@ -1,7 +1,7 @@
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-
+import spark.servlet.SparkApplication;
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 import javax.xml.bind.DatatypeConverter;
@@ -9,21 +9,81 @@ import java.util.ArrayList;
 import java.util.Base64;
 
 import static spark.Spark.get;
+import static spark.Spark.port;
 import static spark.Spark.post;
 
-public class Startup {
+public class Startup implements SparkApplication{
 
     // Create retrofit rest client for CaptainHook
-    public static CaptainHookApiService service = CaptainHookApiService.retrofit.create(CaptainHookApiService.class);
-    public static TokenService tokenService = TokenService.retrofit.create(TokenService.class);
+    private CaptainHookApiService service = CaptainHookApiService.retrofit.create(CaptainHookApiService.class);
+    private TokenService tokenService = TokenService.retrofit.create(TokenService.class);
 
-    public static byte[] Secret ={};
-    static ArrayList<String> alerts = new ArrayList();
-    static ArrayList<String> logs = new ArrayList();
-
+    private byte[] Secret ={};
+    private ArrayList<String> alerts = new ArrayList();
+    private ArrayList<String> logs = new ArrayList();
+    private static int port;
     public static void main(String [] args){
+        if(args.length>0){
+            port=Integer.parseInt(args[0]);
+        }else{
+            port=4567;
+        }
+        port(port);
+        new Startup().init();
+    }
 
+    private void createAlertWebHook() {
+        // Tell CaptainHook that we want to subscribe to Alerts
+        Call getTokenCall=tokenService.createToken(new ClientCredentials("transitapi_prod_postman_client","wimt85!").getMap());
+        getTokenCall.enqueue(new Callback() {
+            @Override
+            public void onResponse(Call call, Response response) {
+                System.out.println(response.code());
+                Call createHookCall =  service.createHook(new Hook("http://localhost:4567/hooks/alerts", "Alerts webhook"),"Bearer "+((Token)response.body()).access_token);
+                createHookCall.enqueue(new Callback() {
+                    @Override
+                    public void onResponse(Call call, Response response2) {
+                        if(response2.isSuccessful()) {
+                            System.out.println("Created Hook Successfully");
+                            logs.add("Created Hook Successfully");
+                        }
+                        else {
+                            System.out.println(response.message());
+                            System.out.println("Something went wrong calling webhook setup. Response code: " + response2.code());
+                            logs.add("Something went wrong calling webhook setup. Response code: " + response2.code());
+                        }
+                    }
 
+                    @Override
+                    public void onFailure(Call call, Throwable t) {
+                        if(t.getCause() == null) {
+                            logs.add("CreateHookCall onFailure called.");
+                        } else {
+                            logs.add(t.getCause().toString());
+                            logs.add(t.getCause().getLocalizedMessage());
+                            logs.add("Error occurred in calling Captain Hook.");
+                        }
+                    }
+                });
+            }
+
+            @Override
+            public void onFailure(Call call, Throwable t) {
+
+            }
+        });
+
+    }
+
+    private String encode(byte[] key, byte[] data) throws Exception {
+        Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
+        SecretKeySpec secret_key = new SecretKeySpec(key, sha256_HMAC.getAlgorithm());
+        sha256_HMAC.init(secret_key);
+        return DatatypeConverter.printBase64Binary(sha256_HMAC.doFinal(data));
+    }
+
+    @Override
+    public void init() {
         System.out.println("Startup class loaded.");
         logs.add("Startup class loaded.");
 
@@ -80,55 +140,5 @@ public class Startup {
 
         // this could be in a post create webhook call
         createAlertWebHook();
-    }
-
-    private static void createAlertWebHook() {
-        // Tell CaptainHook that we want to subscribe to Alerts
-        Call getTokenCall=tokenService.createToken(new ClientCredentials("transitapi_prod_postman_client","wimt85!").getMap());
-        getTokenCall.enqueue(new Callback() {
-            @Override
-            public void onResponse(Call call, Response response) {
-                System.out.println(response.code());
-                Call createHookCall =  service.createHook(new Hook("http://localhost:4567/hooks/alerts", "Alerts webhook"),"Bearer "+((Token)response.body()).access_token);
-                createHookCall.enqueue(new Callback() {
-                    @Override
-                    public void onResponse(Call call, Response response2) {
-                        if(response2.isSuccessful()) {
-                            System.out.println("Created Hook Successfully");
-                            logs.add("Created Hook Successfully");
-                        }
-                        else {
-                            System.out.println(response.message());
-                            System.out.println("Something went wrong calling webhook setup. Response code: " + response2.code());
-                            logs.add("Something went wrong calling webhook setup. Response code: " + response2.code());
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(Call call, Throwable t) {
-                        if(t.getCause() == null) {
-                            logs.add("CreateHookCall onFailure called.");
-                        } else {
-                            logs.add(t.getCause().toString());
-                            logs.add(t.getCause().getLocalizedMessage());
-                            logs.add("Error occurred in calling Captain Hook.");
-                        }
-                    }
-                });
-            }
-
-            @Override
-            public void onFailure(Call call, Throwable t) {
-
-            }
-        });
-
-    }
-
-    private static String encode(byte[] key, byte[] data) throws Exception {
-        Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
-        SecretKeySpec secret_key = new SecretKeySpec(key, sha256_HMAC.getAlgorithm());
-        sha256_HMAC.init(secret_key);
-        return DatatypeConverter.printBase64Binary(sha256_HMAC.doFinal(data));
     }
 }
