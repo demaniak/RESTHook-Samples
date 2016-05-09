4a994cdffeb4d5f30b54396532ddd54ee708dce0
hendrikdelarey <henk@whereismytransport.com>
2016-05-09 11:01:21 +0200

    Update CaptainHookApiService.java

--- src/main/java/com/whereismytransport/resthook/client/CaptainHookApiService.java
+++ src/main/java/com/whereismytransport/resthook/client/CaptainHookApiService.java
@@ -2,6 +2,8 @@ package com.whereismytransport.resthook.client;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.*;
 
 
